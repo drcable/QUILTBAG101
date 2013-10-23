9285f28db0e8708230739ce069ed46dcb3573b5c
drcable <drcable@tarbwf.com>
2013-10-23 01:51:25 +0100

    deploying first ubersimple version

--- QUILTBAG/output/actions.js
+++ QUILTBAG/output/actions.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
     $('body').addClass('js');
     content = $('.content')
-    content.css('left',0);
+    if (window.location.hash){
+        slide = window.location.hash;
+    }else{
+        slide = "#Introduction";
+    }
+    $('nav.topBar ul li a').each(function (i,v){
+        v = $(v);
+        v.click(
+            function( event ){
+                scrollTo(v.attr('href'));
+                event.preventDefault()
+            }
+        );
+    })
+    $('a.sides.right').click(
+            function(event){
+                event.preventDefault();
+                scrollNext()
+            }
+        )
+    $('a.sides.left').click(
+            function(event){
+                event.preventDefault();
+                scrollPrev()
+            }
+        )
 })
-next = function(){
-    content.css('left',parseInt(content.css('left').replace("px","")) - content.width())
+scrollTo = function(id){
+    content.css("left",-1*parseInt($(id).css('left').replace("px","")));
+    slide = id;
+}
+scrollNext = function(){
+    scrollTo("#" + $(slide).next().attr("id"));
+}
+scrollPrev = function(){
+    scrollTo("#" + $(slide).prev().attr("id"));
 }
